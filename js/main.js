857d01436227b9665b628386e3441f1011339caf
sJavi <javierroig97@gmail.com>
2021-12-15 19:30:34 +0100

    init

--- js/main.js
+++ js/main.js
@@ -15,8 +15,40 @@ function showDivHome(){
 function showDivAbout(){
     libs_general_hideAllDivsInDivExceptOne("div-main", "div-about") 
 }
+function showDivHelp(){
+    libs_general_hideAllDivsInDivExceptOne("div-main", "div-help") 
+}
+function changeActive(){
+	var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+	var current = document.getElementById("menu-about");
+	// console.log(current)
+	current.className = current.className.replace("header","header active");
+	console.log(current.className)
+}
+function changeActive1(){
+	var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+	var current = document.getElementById("menu-help");
+	// console.log(current)
+	current.className = current.className.replace("header","header active");
+	console.log(current.className)
+}
+function changeActive2(){
+	var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+	var current = document.getElementById("menu-home");
+	// console.log(current)
+	current.className = current.className.replace("header","header active");
+	console.log(current.className)
+}
 function linkMenuEvents(){
 	document.getElementById("menu-about").addEventListener("click", showDivAbout);
+	document.getElementById("menu-help").addEventListener("click", showDivHelp);
+	document.getElementById("menu-home").addEventListener("click", showDivHome);
+	document.getElementById("menu-about").addEventListener("click", changeActive);
+	document.getElementById("menu-help").addEventListener("click", changeActive1);
+	document.getElementById("menu-home").addEventListener("click", changeActive2);
 }
 
 function mainInit(){
@@ -46,10 +78,6 @@ function mainInit(){
 	time = '2020-12-14T10:00:00.000Z/2021-12-14T10:00:00.000Z'
 	cql_filter = "nuclide = 'Cs-137'"
 	rasterSource.updateParams({'SERVICE':service, 'LAYERS':layer,'TIME':time, 'cql_filter':cql_filter});
-	
-	
-
-
 }
 
 window.onload = function() {
