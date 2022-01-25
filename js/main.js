7b5d9b8fa637459a1074b188feebe368b7ea2779
ana1027 <sanchezfer.ana@gmail.com>
2022-01-25 19:59:48 +0100

    cleaning code

--- js/main.js
+++ js/main.js
@@ -3,26 +3,6 @@ let start;
 let end;
 let nuclide;
 
-/*SIDEBAR FUNCTIONS
-var sidebarState = 0 // close
-function open_closeNav() {
-  if(sidebarState === 0){
-	sidebarState = 1;
-     document.getElementById("mySidenav").style.width = "250px";
-	 document.getElementById("div-home2").style.marginRight = "250px";
-     //document.getElementById("main").style.marginLeft = "250px";
-     //document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  }
-  else {
-	sidebarState = 0;
-     document.getElementById("mySidenav").style.width = "0";
-	 document.getElementById("div-home2").style.marginRight = "0";
-     //document.getElementById("main").style.marginLeft = "0";
-     //document.body.style.backgroundColor = "white";
-  }
-  console.log(sidebarState);
-} */
-
 function showDivHome(){
     libs_general_hideAllDivsInDivExceptOne("div-main", "div-home") 
 }
@@ -57,24 +37,12 @@ function changeActive2(){
 	console.log(current.className)
 }
 function linkMenuEvents(){
-	//document.getElementById("menu-chart").addEventListener("click", open_closeNav);
 	document.getElementById("menu-about").addEventListener("click", showDivAbout);
 	document.getElementById("menu-help").addEventListener("click", showDivHelp);
 	document.getElementById("menu-home").addEventListener("click", showDivHome);
 	document.getElementById("menu-about").addEventListener("click", changeActive);
 	document.getElementById("menu-help").addEventListener("click", changeActive1);
 	document.getElementById("menu-home").addEventListener("click", changeActive2);
-	/*document.querySelectorAll('ul.list input').forEach(item => {
-		item.addEventListener('click', event => {
-		  var label = item.parentNode;
-		  if (item.checked) {
-			label.style.fontWeight = "bold";
-		  } else {
-			label.style.fontWeight = "normal"
-		  }
-		})
-	})*/
-	
 
 }
 
